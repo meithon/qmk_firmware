5ce2e4807ef9ec40721a3a631aa8ae3c36b50223
MeiWagatsuma <wagamei256@gmail.com>
2024-09-29 09:49:03 +0900

    Update adjust

--- keyboards/crkbd/keymaps/meiwagatsuma/keymap.c
+++ keyboards/crkbd/keymaps/meiwagatsuma/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_TOG,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_VOLU, KC_VOLD,
+      KC_LCTL, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_TOG,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_VOLU, KC_VOLD,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX,  _DEL_W,   _VI_B,   _VI_W, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
