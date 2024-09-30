2ceb4f695d58559109744bd4268f96a0deabe74e
MeiWagatsuma <wagamei256@gmail.com>
2024-09-30 20:19:49 +0900

    fix adjust shift logic

--- keyboards/crkbd/keymaps/meiwagatsuma/keymap.c
+++ keyboards/crkbd/keymaps/meiwagatsuma/keymap.c
@@ -375,6 +375,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         // reset the flag
         lower_pressed = false;
+        adjust_flag = false;
       }
       break;
   }
