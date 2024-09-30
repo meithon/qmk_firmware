8055774f2496b9d93b6c39b56e26c26ee341b6f5
MeiWagatsuma <wagamei256@gmail.com>
2024-09-30 20:50:28 +0900

    Fix shift unregister

--- keyboards/crkbd/keymaps/meiwagatsuma/keymap.c
+++ keyboards/crkbd/keymaps/meiwagatsuma/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            _GUI,  _LOWER,  KC_SPC,      KC_ENT,  _SHIFT,  _RAISE
+                                            _GUI,  _LOWER,  KC_SPC,      _ENTER,  _SHIFT,  _RAISE
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -307,7 +307,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case _GUI:
       if (record->event.pressed) {
         if (enable_alt.by_shift) {
-          unregister_code(KC_LCTL);
+          unregister_code(KC_RS);
           register_code(KC_LALT);
         } else {
           register_code(KC_LGUI);
